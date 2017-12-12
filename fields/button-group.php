3c777870aef5cea4547668a63db2a42396cfa5c9
anhtu\tudt@elightup.com <anhtu261091@gmail.com>
2017-12-12 16:36:30 +0700

    update demo : switch + button group version 2

--- fields/button-group.php
+++ fields/button-group.php
@@ -6,32 +6,71 @@
 add_filter( 'rwmb_meta_boxes', 'your_prefix_button_group_demo' );
 function your_prefix_button_group_demo( $meta_boxes ) {
 	$meta_boxes[] = array(
-		'title'  => __( 'Button Group Demo', 'textdomain' ),
+		'title'  => __( 'Button group demo', 'textdomain' ),
 		'fields' => array(
 			array(
-				'id'   => 'button_group',
-				'name' => __( 'Button group demo', 'textdomain' ),
-				'type' => 'button_group',
+				'name'    => esc_html__( 'Text label', 'textdomain' ),
+				'id'      => "button_group_label_text",
+				'type'    => 'button_group',
 				// Array of 'value' => 'Label' pairs for radio options.
 				// Note: the 'value' is stored in meta field, not the 'Label'
 				'options' => array(
-					'value1' => esc_html__( 'Button 1', 'textdomain' ),
-					'value2' => esc_html__( 'Button 2', 'textdomain' ),
-					'value3' => esc_html__( 'Button 3', 'textdomain' ),
-					'value4' => esc_html__( 'Button 4', 'textdomain' ),
-					'value5' => esc_html__( 'Button 5', 'textdomain' ),
+					'button1' => esc_html__( 'Button 1', 'textdomain' ),
+					'button2' => esc_html__( 'Button 2', 'textdomain' ),
+					'button3' => esc_html__( 'Button 3', 'textdomain' ),
+					'button4' => esc_html__( 'Button 4', 'textdomain' ),
+					'button5' => esc_html__( 'Button 5', 'textdomain' ),
 				),
-				// style defause wordpress button
-				'attributes' => array(
-					'class'        => 'button',
+
+				// Display inline? value : true - false
+				'inline' => true,
+
+				// Display multiple? value : true - false
+				// 'multiple'    => true,
+			),
+
+			array(
+				'name'    => esc_html__( 'With icon + multiple choices', 'textdomain' ),
+				'id'      => "button_group_label_icon",
+				'type'    => 'button_group',
+				// Array of 'value' => 'Label' pairs for radio options.
+				// Note: the 'value' is stored in meta field, not the 'Label'
+				'options' => array(
+					'bold' 			=> '<span class="dashicons dashicons-editor-bold"></span>',
+					'italic' 		=> '<span class="dashicons dashicons-editor-italic"></span>',
+					'alignleft' 	=> '<span class="dashicons dashicons-editor-alignleft"></span>',
+					'aligncenter' 	=> '<span class="dashicons dashicons-editor-aligncenter"></span>',
+					'alignright' 	=> '<span class="dashicons dashicons-editor-alignright"></span>',
 				),
+
 				// Display inline? value : true - false
 				'inline' => true,
 
 				// Display multiple? value : true - false
 				'multiple'    => true,
 			),
+
+			array(
+				'name'    => esc_html__( 'Vertical icons', 'textdomain' ),
+				'id'      => "button_group_label_icon_position_inline",
+				'type'    => 'button_group',
+				// Array of 'value' => 'Label' pairs for radio options.
+				// Note: the 'value' is stored in meta field, not the 'Label'
+				'options' => array(
+					'rss' 			=> '<span class="dashicons dashicons-rss"></span>',
+					'twitter' 		=> '<span class="dashicons dashicons-twitter"></span>',
+					'facebook' 		=> '<span class="dashicons dashicons-facebook-alt"></span>',
+					'googleplus' 	=> '<span class="dashicons dashicons-googleplus"></span>',
+				),
+
+				// Display inline? value : true - false
+				'inline' => false,
+
+				// Display multiple? value : true - false
+				'multiple'    => true,
+			),
 		),
+
 	);
 	return $meta_boxes;
 }
--- fields/switch.php
+++ fields/switch.php
@@ -6,18 +6,60 @@
 add_filter( 'rwmb_meta_boxes', 'your_prefix_switch_demo' );
 function your_prefix_switch_demo( $meta_boxes ) {
 	$meta_boxes[] = array(
-		'title'  => __( 'Checkbox Upload Demo', 'textdomain' ),
+		'title'  => __( 'Check switch rounded demo', 'textdomain' ),
 		'fields' => array(
 			array(
-				'id'   => 'switch',
-				'name' => __( 'Switch demo', 'textdomain' ),
+				'type'      => 'switch',
+				'name' 		=> esc_html__( 'Rounded no label', 'textdomain' ),
+				'id'   		=> "switch_rounded",
+				// Value can be 0 or 1
+				'std'  		=> 1,
+
+			),
+			array(
+				'type'      => 'switch',
+				'name' 		=> esc_html__( 'On-Off label', 'textdomain' ),
+				'id'   		=> "switch_rounded_label",
+				// Value can be 0 or 1
+				'std'  		=> 1,
+				'on_label'	=> esc_html__( 'On', 'textdomain' ),
+				'off_label'	=> esc_html__( 'Off', 'textdomain' ),
+			),
+			array(
+				'type'      => 'switch',
+				'name' 		=> esc_html__( 'With icon', 'textdomain' ),
+				'id'   		=> "switch_rounded_label_true",
+				// Value can be 0 or 1
+				'std'  		=> 1,
+				'on_label'	=> '<span class="dashicons dashicons-yes"></span>',
+				'off_label'	=> '<span class="dashicons dashicons-no"></span>',
+			),
+		),
+	);
+	$meta_boxes[] = array(
+		'title'  => __( 'Check switch square demo', 'textdomain' ),
+		'fields' => array(
+			array(
+				'id'   => 'switch_square',
+				'name' => __( 'Square no label', 'textdomain' ),
 				'type' => 'switch',
 				'std'  		=> 1,
 				'style'		=> 'square',
-				'on_label'	=> 'On',
-				'off_label'	=> 'Off',
 			),
+			array(
+				'type'      => 'switch',
+				'name' 		=> esc_html__( 'Enable - Disable', 'textdomain' ),
+				'id'   		=> "switch_square_label",
+				// Value can be 0 or 1
+				'std'  		=> 1,
+				// 2 style: rounded and square
+				'style'		=> 'square',
+				'on_label'	=> esc_html__( 'Enable', 'textdomain' ),
+				'off_label'	=> esc_html__( 'Disable', 'textdomain' ),
+			),
+
 		),
+
 	);
 	return $meta_boxes;
 }
