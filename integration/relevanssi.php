d143f8e16b5a3d2fa017915b69f37786061ef57c
Tran Ngoc Tuan Anh <anhtnt@elightup.com>
2018-09-05 02:50:12 +0000

    Update and rename Relevanssi-integration to integration/relevanssi.php

--- integration/relevanssi.php
+++ integration/relevanssi.php
@@ -1,3 +1,4 @@
+<?php
 add_filter( 'relevanssi_content_to_index', 'rlv_index_attachment_names', 10, 2 );
 function rlv_index_attachment_names( $content, $post_id ) {
 	if ($post_id->post_type == "your_post_type") {
