226fd1a02cb1e2237e2e2ff8c0bfe89f4d7703ed
Tran Ngoc Tuan Anh <anhtnt@elightup.com>
2019-02-13 08:41:45 +0000

    Create edit-user-email.php

---
+++ extensions/mb-user-profile/edit-user-email.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * To let users edit their email, simply create a meta box (or a new field in an existing meta box) with id `user_email`. And then include the meta box into the shortcode.
+ */
+
+add_filter( 'rwmb_meta_boxes', function( $meta_boxes ) {
+	$meta_boxes[] = [
+		'title' => 'Edit User Email',
+		'id' => 'edit-user-email',
+		'type' => 'user',
+		'fields' => [
+			[
+				'type' => 'email',
+				'id'   => 'user_email',
+				'name' => 'Email',
+			]
+		],
+	];
+	return $meta_boxes;
+} );
+
+/**
+ * Then add the following shortcode into a page:
+ *
+ * [mb_user_profile_info id="edit-user-email"]
+ */
